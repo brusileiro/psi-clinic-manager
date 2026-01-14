317c6103102d8a3ce1e11745694674f64559b2a0
brusileiro <brusilribeiro@gmail.com>
2026-01-14 15:53:07 -0300

    Testes: desativa contextLoads até implementação dos testes de integração

--- src/test/java/com/example/PsiSoftware/DemoApplicationTests.java
+++ src/test/java/com/example/PsiSoftware/DemoApplicationTests.java
@@ -1,8 +1,7 @@
-package com.example.PsiSoftware;
-
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Teste de contexto será reativado nos testes de integração")
 @SpringBootTest
 class DemoApplicationTests {
 
